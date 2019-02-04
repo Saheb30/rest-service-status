89f1e689212c34768759022cf52a2244e36ae6b0
Jana <saheb.jana@accenture.com>
2019-02-04 11:31:24 +0530

    Chnages in ServiceStatusImpl

--- src/main/java/org/saheb/model/ServiceStatusImpl.java
+++ src/main/java/org/saheb/model/ServiceStatusImpl.java
@@ -1,5 +1,9 @@
 package org.saheb.model;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
@@ -9,32 +13,44 @@ import javax.ws.rs.core.Response;
 
 import org.saheb.beans.ServiceStatus;
 import org.saheb.beans.ServiceStatusList;
+import org.saheb.util.EmailUtil;
 
 public class ServiceStatusImpl implements ServicesStatus {
-	private static final String REST_URI = "http://google.com";
+	private static final String GOOGLE_URI = "http://google.com";
+	private static final String GITHUB_URI = "https://github.com";
+	private static final Map<String, String> serviceMap = new HashMap<>();
+	static {
+		serviceMap.put("GOOGLE", GOOGLE_URI);
+		serviceMap.put("GITHUB", GITHUB_URI);
+	}
 
 	@Override
 	public ServiceStatusList getServiceStatusList() {
-		ServiceStatusList serviceStatusList = new ServiceStatusList();
-		ServiceStatus serviceStatus = new ServiceStatus();
-		Client client = ClientBuilder.newClient();
-		WebTarget webTarget = client.target(REST_URI);
-		
-		Invocation.Builder invocationBuilder =	webTarget.request(MediaType.APPLICATION_JSON);
-		Response response = invocationBuilder.get();
-		System.out.println("inside service response->"+response.toString());
-		if(response != null && response.getStatus() == 200 && !response.toString().toLowerCase().contains("error")) {
-			serviceStatus.setServiceName("GOOGLE");
-			serviceStatus.setStatus(true);
-			serviceStatus.setStatusCode(response.getStatus());
+		ServiceStatusList serviceStatusList = new ServiceStatusList();;
+		ServiceStatus serviceStatus = null;
+		for(Entry<String, String> entry : serviceMap.entrySet()) {
+			String serviceName = entry.getKey();
+			String serviceURI = entry.getValue();
+			
+			serviceStatus = new ServiceStatus();
+			Client client = ClientBuilder.newClient();
+			WebTarget webTarget = client.target(serviceURI);
+			
+			Invocation.Builder invocationBuilder =	webTarget.request(MediaType.APPLICATION_JSON);
+			Response response = invocationBuilder.get();
+			if(response != null && response.getStatus() >= 200 && response.getStatus() <= 226 && !response.toString().toLowerCase().contains("error")) {
+				serviceStatus.setServiceName(serviceName);
+				serviceStatus.setStatus(true);
+				serviceStatus.setStatusCode(response.getStatus());
+			}else {
+				serviceStatus.setServiceName(serviceName);
+				serviceStatus.setStatus(false);
+				serviceStatus.setStatusCode(response.getStatus());
+			}
 			serviceStatusList.getServiceStatusList().add(serviceStatus);
 		}
+		EmailUtil.sendEmail(serviceStatusList);
 		System.out.println("List size-->"+serviceStatusList.getServiceStatusList().size());
 		return serviceStatusList;
 	}
-	/*public static void main(String[] args) {
-		int n = new ServiceStatusImpl().getServiceStatusList().getServiceStatusList().size();
-		System.out.println(n);
-	}*/
-
 }
---
+++ src/main/java/org/saheb/util/EmailUtil.java
@@ -0,0 +1,9 @@
+package org.saheb.util;
+
+import org.saheb.beans.ServiceStatusList;
+
+public class EmailUtil {
+	public static boolean sendEmail(ServiceStatusList serviceStatusList) {
+		return false;
+	}
+}
